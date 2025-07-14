dd26da6edd377c6c23aaab65c7ac6b6b468433ed
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-07-14 00:49:06 -0300

    InicioEmprestimo

--- src/model/entidades/Emprestimo.ts
+++ src/model/entidades/Emprestimo.ts
@@ -2,27 +2,27 @@ export class Emprestimo{
     id:number;
     cpf_usuario:string;
     isbn_livro:string;
-    data_emprestimo:Date;
-    data_devolucao:Date;
+    data_emprestimo:Date|null;
+    data_devolucao:Date|null;
     data_entrega:Date|null;
     dias_atraso:number;
-    suspensao_ate:Date;
+    suspensao_ate:Date|null;
     
-    constructor(id:number,
-    cpf_usuario:string,
-    isbn_livro:string,
-    data_emprestimo:Date,
-    data_devolucao:Date,
-    data_entrega:Date|null,
-    dias_atraso:number,
-    suspensao_ate:Date){
-        this.id = id;
-        this.cpf_usuario = cpf_usuario;
-        this.isbn_livro = isbn_livro;
-        this.data_emprestimo = data_emprestimo;
-        this.data_devolucao = data_devolucao;
-        this.data_entrega = data_entrega;
-        this.dias_atraso = dias_atraso;
-        this.suspensao_ate = suspensao_ate;
+    constructor(id?:number,
+    cpf_usuario?:string,
+    isbn_livro?:string,
+    data_emprestimo?:Date|null,
+    data_devolucao?:Date|null,
+    data_entrega?:Date|null,
+    dias_atraso?:number,
+    suspensao_ate?:Date|null){
+        this.id = id||0;
+        this.cpf_usuario = cpf_usuario||'';
+        this.isbn_livro = isbn_livro||'';
+        this.data_emprestimo = data_emprestimo||null;
+        this.data_devolucao = data_devolucao||null;
+        this.data_entrega = data_entrega||null;
+        this.dias_atraso = dias_atraso||0;
+        this.suspensao_ate = suspensao_ate||null;
     }
 }
